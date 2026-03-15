c958fa15466487edda87a03e58003475447373d0
arshiel <arselo56@gmail.com>
2026-03-15 20:26:29 +0700

    Introduction to Python

--- day1.py
+++ day1.py
@@ -1,33 +1,33 @@
-#Introduction
+# Introduction to Python
 
-print("Saya sedang belajar Python")
+print("===== Latihan 1 =====")
+print("Nama: Arshiel")
+print("Universitas: Umsida")
+print("Jurusan: Informatika")
+print("Alasan belajar Python: ingin menjadi data analyst")
 
-print("Hasil perhitungan:")
-print(10 + 5)
-print(10 * 5)
-print(10 / 2)
-
-print("Cek tipe data:")
-print(type(100))
-print(type(3.14))
-print(type("Data Science"))
-print(type(False))
-
-umur = 22
-umur_dalam_hari = umur * 365
-
-print("Umur dalam hari:", umur_dalam_hari)
-
-print("Nama saya Arshiel")
+print("\n===== Latihan 2 =====")
 print("Saya belajar Python")
+print("Python itu menyenangkan")
+print("Saya akan konsisten belajar setiap hari")
 
-panjang = 10
-lebar = 5
-luas = panjang * lebar
+print("\n===== Tantangan =====")
+print("Python" * 5)
+# 1. Outputnya adalah teks "Python" sebanyak 5 kali
+# 2. Karena Python mendukung operasi perkalian pada string
 
-print("Luas persegi panjang adalah", luas)
+print("\nMini Latihan")
 
-nilai = [80, 75, 90, 85]
-rata = sum(nilai) / len(nilai)
+print("Data" * 3)
+print(3 * "AI")
+print("Python " * 4)
+# 1. Output: DataDataData, AIAIAI, Python Python Python Python
+# 2. Spasi setelah "Python " membuat hasil pengulangan memiliki jarak
 
-print("Rata-rata nilai:", rata)
+
+print("\nTantangan")
+
+print("=" * 15)
+print("BELAJAR PYTHON")
+print("=" * 15)
+
